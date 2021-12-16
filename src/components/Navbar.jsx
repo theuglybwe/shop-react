7862e62d973b9a5069902ebb5f3cb76d4dd9b595
theuglybwe <87030539+theuglybwe@users.noreply.github.com>
2021-12-16 09:46:43 +0200

    styled component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+//backtics here
+const Container = styled.div`
+ height:60px;
+ background-color: black;
+`
+
+
+
+
+
+
+const Navbar = () => {
+    return (
+        <Container>
+            navbar
+        </Container>
+    )
+}
+
+export default Navbar
