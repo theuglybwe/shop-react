5b403079b4f2b648272a882b9691aba875bad2cc
theuglybwe <87030539+theuglybwe@users.noreply.github.com>
2021-12-21 11:08:25 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,50 @@
 
+import { Facebook, Instagram, Twitter } from '@material-ui/icons';
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+display: flex;`
+const Logo = Styled.h1``;
+const Desc = styled.p``;
+
+const SocialContainer = styled.div``;
+
+const SocialIcon = styled.div``;
+
+
+
+const Left = styled.div`
+flex:1;
+`
+const Center = styled.div`
+flex:1;
+`
+const Right = styled.div`
+flex:1;
+`
 
 const Footer = () => {
     return (
         <Container>
-            
+            <Left>
+                <Logo>UNDYING</Logo>
+                <Desc>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque consequuntur a fuga libero excepturi explicabo, illum harum nam tenetur, vero necessitatibus? Tenetur eius illum voluptates, quis voluptatibus ipsum sint voluptatem!
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon>
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon>
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon>
+                        <Twitter />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center></Center>
+            <Right></Right>
         </Container>
     )
 }
