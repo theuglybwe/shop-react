9761749f6dc5dd0e10c15764456a8f54716cba67
theuglybwe <87030539+theuglybwe@users.noreply.github.com>
2021-12-22 11:10:30 +0200

    Adding more users

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Cart from "./pages/Cart";
 import  Home from "./pages/Home";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 
 const App = () => {
-  return <Product/>
+  return <Cart/>
 };
 
 export default App;
