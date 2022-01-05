835aca1b9f81f0089368eb9786dec44daf2f14cc
theuglybwe <87030539+theuglybwe@users.noreply.github.com>
2022-01-05 11:18:16 +0200

    back end user route

---
+++ EccomerceApi/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB connection successful")
+).catch((err)=>{
+    console.log(err);
+});
+
+app.use("/api/user",userRoute);
+
+app.listen(process.env.PORT || 5000, () =>{
+   console.log("Backend server is running!")
+
+})
